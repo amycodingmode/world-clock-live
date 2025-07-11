dc42afeede781f60f8dea3f1485c38888a65d72c
amycodingmode <giantmotion@gmail.com>
2025-07-11 15:38:23 -0700

    Added current location option

--- src/script.js
+++ src/script.js
@@ -41,6 +41,9 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  } // Default to user's current timezone
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
